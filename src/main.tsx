86ed654a8af53c31e7f4ceb6945da44346768b83
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-08 11:32:55 +0330

    install theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <App />
+      </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
