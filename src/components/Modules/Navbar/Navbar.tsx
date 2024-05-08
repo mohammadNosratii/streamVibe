a9d445bc96d323010693df9424989d4278ccf7b3
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-08 14:09:17 +0330

    complete navbar and responsive

--- src/components/Modules/Navbar/Navbar.tsx
+++ src/components/Modules/Navbar/Navbar.tsx
@@ -6,10 +6,12 @@ export default function Navbar() {
         src="icons/logo-desktop.svg"
         alt=""
       />
-      <img className="md:hidden w-12 h-12" src="icons/bars-3.svg" alt="" />
-      <ul className="hidden md:flex items-center gap-4 2xl:gap-7.5 bg-black-6 py-2 ps-2 pe-8 2xl:py-2.5 2xl:ps-2.5 2xl:pe-10 rounded-xl border-4 border-black-12 child:text-lg child:text-gray-75">
-        <li className="bg-black-10 py-3.5 px-6 rounded-lg border-1 border-transparent">
-          <a className="text-white" href="#">
+      <div className="md:hidden flex items-center justify-center w-12 h-12 bg-black-10 border-3 border-black-15">
+        <img className="w-6 h-6" src="icons/bars-3.svg" alt="" />
+      </div>
+      <ul className="hidden md:flex items-center gap-4 2xl:gap-7.5 bg-black-6 py-2 ps-2 pe-8 2xl:py-2.5 2xl:ps-2.5 2xl:pe-10 rounded-xl border-4 border-black-12 child:text-sm child:lg:text-lg child:text-gray-75 child font-normal">
+        <li className="bg-black-10 py-3 px-5 2xl:py-3.5 2xl:px-6 rounded-lg border-1 border-transparent">
+          <a className="text-white font-medium" href="#">
             Home
           </a>
         </li>
