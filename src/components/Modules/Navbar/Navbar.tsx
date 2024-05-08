128e7802f7ad53807c9a843f17f3281604d6b7c6
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-08 13:08:56 +0330

    changes on navbar

--- src/components/Modules/Navbar/Navbar.tsx
+++ src/components/Modules/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 export default function Navbar() {
   return (
     <div className="container flex items-center py-8 justify-between fixed left-0 right-0">
-      <img className="3xl:w-40 3xl:h-12" src="icons/logo-desktop.svg" alt="" />
+      <img
+        className="w-28 h-12 2xl:w-40 2xl:h-12 4xl:w-48 4xl:h-14"
+        src="icons/logo-desktop.svg"
+        alt=""
+      />
 
       <ul className="flex items-center gap-7.5 bg-black-6 py-2.5 ps-2.5 pe-10 rounded-xl border-4 border-black-12 child:text-lg">
         <li className="bg-black-10 py-3.5 px-6 rounded-lg border-1 border-transparent">
