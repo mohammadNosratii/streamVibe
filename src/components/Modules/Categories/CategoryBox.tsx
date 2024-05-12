4f6404bb8a775a3a06240548ba329effe96d6036
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-12 22:58:27 +0330

    few change on category slider

--- src/components/Modules/Categories/CategoryBox.tsx
+++ src/components/Modules/Categories/CategoryBox.tsx
@@ -3,10 +3,12 @@ import { categoryProps } from "../../../interfaces/category.interface";
 
 export default function CategoryBox({ img, title }: categoryProps) {
   return (
-    <div className="bg-black-10 p-7 border border-black-15 rounded-xl">
+    <div className="dark:bg-black-10 p-5 lg:p-6 2xl:p-7 border border-black-15 rounded-xl space-y-1">
       <img className="w-full object-cover" src={img} alt="" />
       <div className="flex items-center justify-between">
-        <span className="text-lg">{title}</span>
+        <span className="dark:text-white text-sm lg:text-base 2xl:text-lg">
+          {title}
+        </span>
         <ArrowRight />
       </div>
     </div>
