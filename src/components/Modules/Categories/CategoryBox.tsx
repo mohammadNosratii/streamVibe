a317834283429245034b86ff852a477fbf09cdc6
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-14 21:50:00 +0330

    change on category complete

--- src/components/Modules/Categories/CategoryBox.tsx
+++ src/components/Modules/Categories/CategoryBox.tsx
@@ -1,16 +1,29 @@
 import { ArrowRight } from "../../../assets/icons/Arrows";
 import { categoryProps } from "../../../interfaces/category.interface";
+import { Card, CardFooter, Image, Button } from "@nextui-org/react";
 
 export default function CategoryBox({ img, title }: categoryProps) {
   return (
-    <div className="bg-white dark:bg-black-10 bg-gradient-to-tr from-red-45/0 from-80% to-red-45/20 tp p-5 lg:p-6 2xl:p-7 rounded-xl space-y-3">
-      <img className="w-full object-cover" src={img} alt="" />
-      <div className="flex items-center justify-between">
-        <span className="dark:text-white text-sm lg:text-base 2xl:text-lg">
-          {title}
-        </span>
-        <ArrowRight />
-      </div>
-    </div>
+    <Card
+      isFooterBlurred
+      className="w-full h-[300px] shadow-none rounded-[30px] col-span-12 sm:col-span-7 border dark:border-none dark:bg-gradient-to-b dark:from-white/70 dark:to-black-6/0 to-90% bg-gradient-to-t from-black-6/60 to-black-6/10"
+    >
+      <Image
+        removeWrapper
+        alt="Relaxing app background"
+        className="z-0 w-full h-full object-cover pt-1 px-1 rounded-[30px]"
+        src={img}
+      />
+      <CardFooter className="absolute bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+        <div className="flex justify-between flex-grow gap-2 items-center">
+          <div className="flex flex-col">
+            <p className="text-2xl text-white/60">{title}</p>
+          </div>
+        </div>
+        <Button className="dark:bg-gradient-to-tr dark:from-black-6 dark:to-black-6/0" radius="full" size="sm">
+          <ArrowRight />
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
--- src/mock/categoriesInfo.ts
+++ src/mock/categoriesInfo.ts
@@ -1,15 +1,15 @@
-import categoryContainer1 from "../assets/images/categoryContainer1.png";
-import categoryContainer2 from "../assets/images/categoryContainer2.png";
-import categoryContainer3 from "../assets/images/categoryContainer3.png";
-import categoryContainer4 from "../assets/images/categoryContainer4.png";
-import categoryContainer5 from "../assets/images/categoryContainer5.png";
+import venom from "../assets/images/venom.jpg";
+import after from "../assets/images/after.jpg";
+import tedLasso from "../assets/images/tedLasso.webp";
+import badMoms from "../assets/images/badMoms.jpg";
+import dontBreath from "../assets/images/dontBreath.jpg";
 
 const categoriesInfo = [
-  { img: categoryContainer1, title: "Action" },
-  { img: categoryContainer2, title: "Adventure" },
-  { img: categoryContainer3, title: "Comedy" },
-  { img: categoryContainer4, title: "Drama" },
-  { img: categoryContainer5, title: "Horror" },
+  { img: venom, title: "Action" },
+  { img: after, title: "Drama" },
+  { img: tedLasso, title: "Sport" },
+  { img: badMoms, title: "Comedy" },
+  { img: dontBreath, title: "Horror" },
 ];
 
 export default categoriesInfo;
