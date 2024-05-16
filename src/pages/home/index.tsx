2ab9b64555c9acadc745a385e99931bbd72fe2d4
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-16 20:36:09 +0330

    a change

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import Devices from "../../components/Templates/Home/Devices/Devices";
 import getMoviesInfo from "../../mock/getMoviesInfo";
 import devicesBoxInfo from "../../mock/devicesBoxInfo"
 import Subscription from "../../components/Modules/Subscription/Subscription";
-import subscriptionPlansData from "../../mock/subscriptionPlan";
+import subscriptionPlansData from "../../mock/subscriptionPlansData";
 
 export default function Home() {
   return (
