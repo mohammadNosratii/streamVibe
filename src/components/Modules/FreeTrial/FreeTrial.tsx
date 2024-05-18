f932edcc7a5cdd55bbb4acd9a601e37a7ea122fa
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-19 00:07:28 +0330

    a change on freeTrailBox

--- src/components/Modules/FreeTrial/FreeTrial.tsx
+++ src/components/Modules/FreeTrial/FreeTrial.tsx
@@ -5,7 +5,7 @@ export default function FreeTrial() {
     <div className="container mb-32">
       <div className="freeTrail rounded-lg py-12 px-8 lg:py-20 lg:px-[60px] flex flex-col md:flex-row items-center justify-between gap-12 md:gap-16 lg:gap-20 2xl:gap-[100px]">
         <div className="text-center space-y-2.5 md:text-start md:space-y-1">
-          <h1 className="text-2xl lg:text-3xl 2xl:text-5xl">
+          <h1 className="text-2xl lg:text-3xl 2xl:text-5xl text-white">
             Start your free trial today!
           </h1>
           <p className="text-gray-60 text-sm lg:text-base 2xl:text-lg">
