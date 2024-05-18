fa5dec70b1d7b04da96da1ec837f46d2f387bd52
mohammad <user01@USER01s-MacBook-Air.local>
2024-05-18 23:42:14 +0330

    fix sidebar close btn

--- src/components/Modules/Navbar/NavbarVertical.tsx
+++ src/components/Modules/Navbar/NavbarVertical.tsx
@@ -3,19 +3,24 @@ import { Link, Image, User, Divider } from "@nextui-org/react";
 import logo from "/icons/logo-mobile.svg";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 import profile from "../../../assets/images/joker.webp";
+import { navbarShown } from "../../../interfaces/navbarShown.interface";
 
 export default function NavbarVertical({
   isNavbarShown,
-}: {
-  isNavbarShown: boolean;
-}) {
+  setShowNavbarVertical,
+}: navbarShown) {
   return (
     <>
       {isNavbarShown && (
-        <div className="fixed inset-0 bg-black-6/40 z-40"></div>
+        <div
+          onClick={() => setShowNavbarVertical(false)}
+          className="fixed inset-0 bg-black-6/80 z-40"
+        ></div>
       )}
       <div
-        className={`w-[220px] fixed ${isNavbarShown ? "left-0" : "-left-96"} h-screen bg-white/60 dark:bg-black-6/50 backdrop-blur-2xl z-50 transition-all duration-500 pt-6 pb-2 px-3 flex flex-col justify-between md:hidden`}
+        className={`w-[220px] fixed ${
+          isNavbarShown ? "left-0" : "-left-96"
+        } h-screen bg-white/60 dark:bg-black-6/50 backdrop-blur-2xl z-50 transition-all duration-500 pt-6 pb-2 px-3 flex flex-col justify-between md:hidden`}
       >
         <div>
           <div className="flex items-center justify-between">
@@ -26,7 +31,6 @@ export default function NavbarVertical({
           <ul className="space-y-2 mb-4">
             {navbarVerticalItems.map((item, index) => (
               <li key={index}>
-                
                 <Link
                   className={
                     item.route === window.location.pathname
---
+++ src/interfaces/navbarShown.interface.ts
@@ -0,0 +1,6 @@
+interface navbarShown {
+  isNavbarShown: boolean;
+  setShowNavbarVertical: (value: boolean) => void;
+}
+
+export type {navbarShown}
