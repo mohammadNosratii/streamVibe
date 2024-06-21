5f62b33db86010175e37e75d8c3bf56998b5cb64
mohammadNosratii <mohammadnosratii2002@gmail.com>
2024-06-21 14:31:18 +0330

    change

--- src/components/Templates/Movies/MoviesBoxWrapper.tsx
+++ src/components/Templates/Movies/MoviesBoxWrapper.tsx
@@ -11,6 +11,7 @@ export default function MoviesBoxWrapper({
       <div className="border border-gray-90 dark:border-black-15 p-12 rounded-xl relative">
         <div className="text-white px-4 py-2 bg-red-45 rounded-lg absolute top-0 -translate-y-1/2 left-12">
           Movies
+          
         </div>
         <div className="space-y-24">
           {moviesCardInfo.map((category, index) => (
