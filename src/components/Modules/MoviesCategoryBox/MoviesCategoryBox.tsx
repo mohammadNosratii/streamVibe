334c7458683d31d9fdd181bd0f8c897289cf7d04
mohammad <user01@USER01s-MacBook-Air.local>
2024-06-21 14:08:02 +0330

    a change

--- src/components/Modules/MoviesCategoryBox/MoviesCategoryBox.tsx
+++ src/components/Modules/MoviesCategoryBox/MoviesCategoryBox.tsx
@@ -10,8 +10,10 @@ export default function MoviesCategoryBox({
   return (
     <div>
       <div className="flex items-center justify-between">
-        <h1 className="text-xl 2xs:text-2xl 3xs:text-3xl sm:text-4xl">{category}</h1>
-        <Link className="flex items-center gap-2" to="/">
+        <h1 className="text-xl 2xs:text-2xl 3xs:text-3xl sm:text-4xl">
+          {category}
+        </h1>
+        <Link className="flex items-center gap-2 hover:text-red-45 transition-all duration-300" to="/">
           <span className="text-sm 3xs:text-base sm:text-lg">See All</span>
           <ArrowRight />
         </Link>
--- src/components/Templates/Movies/MoviesBoxWrapper.tsx
+++ src/components/Templates/Movies/MoviesBoxWrapper.tsx
@@ -8,7 +8,7 @@ export default function MoviesBoxWrapper({
 }) {
   return (
     <div className="container my-20 max-w-[100%]">
-      <div className="border border-black-15 p-12 rounded-xl relative">
+      <div className="border border-gray-90 dark:border-black-15 p-12 rounded-xl relative">
         <div className="text-white px-4 py-2 bg-red-45 rounded-lg absolute top-0 -translate-y-1/2 left-12">
           Movies
         </div>
