b4c041955e9d312a9f49f3e6595a39d93f8d97e5
Aryan kholqi <aryankholqi@gmail.com>
2024-06-23 23:09:12 +0330

    fix vercel

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,7 +1,7 @@
 import FreeTrial from "../../components/Modules/FreeTrial/FreeTrial";
 import NavigationSlider from "../../components/Modules/Sliders/NavigationSlider";
 import MoviesBoxWrapper from "../../components/Templates/Movies/MoviesBoxWrapper";
-import moviesCardInfo from "../../mock/moviesCardInfo";
+import moviesCardInfo from "../../mock/movies";
 import newMoviesInfo from "../../mock/newMoviesInfo";
 
 export default function Movies() {
