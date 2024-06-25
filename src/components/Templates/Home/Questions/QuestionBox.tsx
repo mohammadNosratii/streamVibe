fe298807290e41faf9555b763a4761cf62237b9b
Aryan kholqi <aryankholqi@gmail.com>
2024-06-25 22:48:40 +0330

    few change

--- src/components/Templates/Home/Questions/QuestionBox.tsx
+++ src/components/Templates/Home/Questions/QuestionBox.tsx
@@ -8,7 +8,7 @@ export default function QuestionBox({ data }: { data: questionBoxProps[] }) {
         <div className='grid grid-cols-1 lg:grid-cols-2 mt-16 w-[95%] mx-auto lg:gap-28'>
             <Accordion>
                 {data.slice(0, 4).map((question) => (
-                    <AccordionItem key={question.id} aria-label={question.title} title={question.title} indicator={({ isOpen }) => (isOpen ? <MinusIcon /> : <PlusIcon />)} startContent={
+                    <AccordionItem key={question.id} aria-label={question.title} title={question.title} indicator={({ isOpen }) => (isOpen ? <MinusIcon /> : <PlusIcon className='dark:fill-current' />)} startContent={
                         <Card className='bg-white dark:bg-black-15 w-12 h-12 flex items-center justify-center shadow-sm'>{question.id.toString().padStart(2, '0')}</Card>
                     }>
                         {question.description}
@@ -17,7 +17,7 @@ export default function QuestionBox({ data }: { data: questionBoxProps[] }) {
             </Accordion>
             <Accordion>
                 {data.slice(4, 8).map((question) => (
-                    <AccordionItem key={question.id} aria-label={question.title} title={question.title} indicator={({ isOpen }) => (isOpen ? <MinusIcon /> : <PlusIcon />)} startContent={
+                    <AccordionItem key={question.id} aria-label={question.title} title={question.title} indicator={({ isOpen }) => (isOpen ? <MinusIcon /> : <PlusIcon className='dark:fill-current'/>)} startContent={
                         <Card className='bg-white dark:bg-black-15 w-12 h-12 flex items-center justify-center shadow-sm'>{question.id.toString().padStart(2, '0')}</Card>
                     }>
                         {question.description}
