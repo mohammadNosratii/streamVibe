6d9f7b1af6aff72c96c5b25356a1bf34b7ec20c4
Aryan kholqi <aryankholqi@gmail.com>
2024-06-25 12:32:52 +0330

    implement tasntack/query

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "./index.css";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>
