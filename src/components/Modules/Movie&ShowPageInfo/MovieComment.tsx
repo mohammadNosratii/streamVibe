82d0af46d41fddf3fb717ffcbd4f9b03a9a7c4a2
Aryan kholqi <aryankholqi@gmail.com>
2024-07-09 13:53:13 +0330

    implement add review for movie

--- src/components/Modules/Movie&ShowPageInfo/MovieComment.tsx
+++ src/components/Modules/Movie&ShowPageInfo/MovieComment.tsx
@@ -2,12 +2,19 @@ import { Button } from "@nextui-org/react";
 import PlusIcon from "../../../assets/icons/Plus";
 import MovieCommentBox from "./MovieCommentBox";
 import { allMoviesData } from "../../../interfaces/allMoviesData.interface";
+import AddReviewForm from "./AddReviewForm";
+import { useState } from "react";
 
 export default function MovieComment({
   movieData,
 }: {
   movieData: allMoviesData;
 }) {
+  const [addReviewTrigger, setAddReviewTrigger] = useState(false)
+
+  const triggerAddReviewHandler = () => {
+    setAddReviewTrigger((prev) => !prev)
+  }
   return (
     <div className="space-y-10 dark:bg-black-10 border-2 dark:border-black-15 p-6 md:p-12 rounded-2xl">
       <div className="flex items-center justify-between">
@@ -17,9 +24,13 @@ export default function MovieComment({
         <Button
           className="rounded-lg dark:bg-black-8 p-3 md:py-3.5 md:px-4 text-sm md:text-lg"
           startContent={<PlusIcon className="fill-black dark:fill-white" />}
+          onClick={triggerAddReviewHandler}
         >
           Add Your Review
         </Button>
+      </div>
+      <div className={`transition-all duration-250 ${addReviewTrigger ? "h-[260px]" : "h-[0px]"} overflow-hidden`}>
+        <AddReviewForm />
       </div>
       {movieData.comments.map((comment, index) => (
         <MovieCommentBox key={index} {...comment} />
