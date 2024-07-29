b0d9cb8cdfbc5a4ffb3fd2348605340a345b7b2f
mohammadNosratii <mohammadnosratii2002@gmail.com>
2024-07-29 15:19:31 +0330

    a change

--- src/components/Templates/Support/ContactUs.tsx
+++ src/components/Templates/Support/ContactUs.tsx
@@ -1,23 +1,33 @@
 import { Image } from "@nextui-org/react";
-import contactUs from "../../../assets/images/contactUs.png"
+import contactUs from "../../../assets/images/contactUs.png";
 import ContactUsForm from "./ContactUsForm";
 
 export default function ContactUs() {
-    return (
-        <div className="grid grid-cols-12 gap-6">
-            <div className="col-span-5 flex flex-col gap-4">
-                <h1 className="font-manropeBold text-5xl leading-[60px]">Welcome to our support page!</h1>
-                <p className="font-manropeRegular text-gray-60">We're here to help you with any problems you may be having with our product.</p>
-                <div className="bg-black-15 p-2 rounded-xl">
-                    <Image alt="" src={contactUs} className="bg-black-6 w-full" classNames={{
-                        wrapper: ["!max-w-none"]
-                    }} />
-                </div>
-            </div>
-            {/* //TODO should update height of inputs like figma */}
-            <div className="col-span-7 bg-white dark:bg-black-6 border-2 dark:border-black-15 rounded-xl px-5 py-10">
-                <ContactUsForm />
-            </div>
+  return (
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-5 flex flex-col gap-4">
+        <h1 className="font-manropeBold text-5xl leading-[60px]">
+          Welcome to our support page!
+        </h1>
+        <p className="font-manropeRegular text-gray-60">
+          We're here to help you with any problems you may be having with our
+          product.
+        </p>
+        <div className="bg-black-15 p-2 rounded-xl">
+          <Image
+            alt=""
+            src={contactUs}
+            className="bg-black-6 w-full"
+            classNames={{
+              wrapper: ["!max-w-none"],
+            }}
+          />
         </div>
-    )
+      </div>
+      {/* //TODO should update height of inputs like figma */}
+      <div className="col-span-7 bg-white h-max dark:bg-black-6 border-2 dark:border-black-15 rounded-xl px-5 py-10">
+        <ContactUsForm />
+      </div>
+    </div>
+  );
 }
--- src/components/Templates/Support/ContactUsForm.tsx
+++ src/components/Templates/Support/ContactUsForm.tsx
@@ -1,124 +1,146 @@
-import { SubmitHandler, useForm } from "react-hook-form"
-import { contactUsProps } from "../../../interfaces/contactUs.interface"
+import { SubmitHandler, useForm } from "react-hook-form";
+import { contactUsProps } from "../../../interfaces/contactUs.interface";
 import { Button, Checkbox, Input, Textarea } from "@nextui-org/react";
 import { emailRegex } from "../../../utils/combineEmailAndPhoneRegex";
 import AutoCompletePhone from "../../Modules/AutoCompletePhone/AutoCompletePhone";
 import { useMemo, useState } from "react";
 
 export default function ContactUsForm() {
-    const { register, handleSubmit, formState: { errors } } = useForm<contactUsProps>()
-    const [isAccetpedRules, setIsAcceptedRules] = useState<boolean>(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<contactUsProps>();
+  const [isAccetpedRules, setIsAcceptedRules] = useState<boolean>(false);
 
-    const submitFormHandler: SubmitHandler<contactUsProps> = (data) => {
-        console.log(data);
-    }
-    return (
-        <form onSubmit={handleSubmit(submitFormHandler)} className="space-y-8">
-            {useMemo(() => (
-                <div className="grid grid-cols-2 gap-8">
-                    <Input
-                        classNames={{
-                            mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
-                            inputWrapper: [
-                                "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
-                            ],
-                            input: ["bg-transparent"],
-                            innerWrapper: ["bg-transparent"],
-                        }}
-                        size="sm"
-                        label="First name"
-                        variant="bordered"
-                        {...register("firstName", {
-                            required: "First name could not be empty",
-                        })}
-                        errorMessage={errors.firstName?.message}
-                        isInvalid={Boolean(errors.firstName)}
-                    />
-                    <Input
-                        classNames={{
-                            mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
-                            inputWrapper: [
-                                "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
-                            ],
-                            input: ["bg-transparent"],
-                            innerWrapper: ["bg-transparent"],
-                        }}
-                        size="sm"
-                        label="Last name"
-                        variant="bordered"
-                        {...register("lastName", {
-                            required: "Last name could not be empty",
-                        })}
-                        errorMessage={errors.lastName?.message}
-                        isInvalid={Boolean(errors.lastName)}
-                    />
-                    <Input
-                        classNames={{
-                            mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
-                            inputWrapper: [
-                                "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
-                            ],
-                            input: ["bg-transparent"],
-                            innerWrapper: ["bg-transparent"],
-                        }}
-                        size="sm"
-                        label="Email"
-                        type="email"
-                        variant="bordered"
-                        {...register("email", {
-                            required: "Email could not be empty",
-                            pattern: {
-                                value: emailRegex,
-                                message: "Email is not valid"
-                            }
-                        })}
-                        errorMessage={errors.email?.message}
-                        isInvalid={Boolean(errors.email)}
-                    />
-                    <Input
-                        startContent={<AutoCompletePhone />}
-                        classNames={{
-                            mainWrapper: ["bg-transparent outline-none rounded-2xl"],
-                            inputWrapper: [
-                                "bg-transparent border-1 dark:border-black-15 rounded-2xl h-full ps-0 pe-3",
-                            ],
-                            input: ["bg-transparent"],
-                            innerWrapper: ["bg-transparent"],
-                        }}
-                        type="number"
-                        placeholder="Phone Number"
-                        {...register("phoneNumber")}
-                    />
-                    <Textarea
-                        classNames={{
-                            base: ["col-span-2"],
-                            mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
-                            inputWrapper: [
-                                "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
-                            ],
-                            input: ["bg-transparent"],
-                            innerWrapper: ["bg-transparent"],
-                        }}
-                        size="sm"
-                        label="Message"
-                        variant="bordered"
-                        {...register("message", {
-                            required: "Message could not be empty",
-                        })}
-                        errorMessage={errors.message?.message}
-                        isInvalid={Boolean(errors.message)}
-                    />
-                </div>
-            ), [errors.email, errors.firstName, errors.lastName, errors.message, register])}
-            <div className="flex justify-between items-center">
-                <Checkbox
-                    isSelected={isAccetpedRules}
-                    onValueChange={(newValue) => setIsAcceptedRules(newValue)}
-                    classNames={{
-                        label: ["text-sm text-gray-60"]
-                    }}>I agree with Terms of Use and Privacy Policy</Checkbox>
-                <Button className="text-white bg-red-45 dark:bg-red-45/80" type="submit" isDisabled={!isAccetpedRules}>Send Message</Button>
-            </div>
-        </form>
-    )
+  const submitFormHandler: SubmitHandler<contactUsProps> = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(submitFormHandler)} className="space-y-8 h-max">
+      {useMemo(
+        () => (
+          <div className="grid grid-cols-2 gap-8">
+            <Input
+              classNames={{
+                mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
+                inputWrapper: [
+                  "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
+                ],
+                input: ["bg-transparent"],
+                innerWrapper: ["bg-transparent"],
+              }}
+              size="sm"
+              label="First name"
+              variant="bordered"
+              {...register("firstName", {
+                required: "First name could not be empty",
+              })}
+              errorMessage={errors.firstName?.message}
+              isInvalid={Boolean(errors.firstName)}
+            />
+            <Input
+              classNames={{
+                mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
+                inputWrapper: [
+                  "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
+                ],
+                input: ["bg-transparent"],
+                innerWrapper: ["bg-transparent"],
+              }}
+              size="sm"
+              label="Last name"
+              variant="bordered"
+              {...register("lastName", {
+                required: "Last name could not be empty",
+              })}
+              errorMessage={errors.lastName?.message}
+              isInvalid={Boolean(errors.lastName)}
+            />
+            <Input
+              classNames={{
+                mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
+                inputWrapper: [
+                  "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
+                ],
+                input: ["bg-transparent"],
+                innerWrapper: ["bg-transparent"],
+              }}
+              size="sm"
+              label="Email"
+              type="email"
+              variant="bordered"
+              {...register("email", {
+                required: "Email could not be empty",
+                pattern: {
+                  value: emailRegex,
+                  message: "Email is not valid",
+                },
+              })}
+              errorMessage={errors.email?.message}
+              isInvalid={Boolean(errors.email)}
+            />
+            <Input
+              startContent={<AutoCompletePhone />}
+              classNames={{
+                mainWrapper: ["bg-transparent outline-none rounded-2xl"],
+                inputWrapper: [
+                  "bg-transparent border-1 dark:border-black-15 rounded-2xl h-full ps-0 pe-3",
+                ],
+                input: ["bg-transparent"],
+                innerWrapper: ["bg-transparent"],
+              }}
+              type="number"
+              placeholder="Phone Number"
+              {...register("phoneNumber")}
+            />
+            <Textarea
+              classNames={{
+                base: ["col-span-2"],
+                mainWrapper: ["bg-transparent outline-noen rounded-2xl"],
+                inputWrapper: [
+                  "bg-transparent border-2 dark:border-[#3f3f46] rounded-2xl",
+                ],
+                input: ["bg-transparent"],
+                innerWrapper: ["bg-transparent"],
+              }}
+              size="sm"
+              label="Message"
+              variant="bordered"
+              {...register("message", {
+                required: "Message could not be empty",
+              })}
+              errorMessage={errors.message?.message}
+              isInvalid={Boolean(errors.message)}
+            />
+          </div>
+        ),
+        [
+          errors.email,
+          errors.firstName,
+          errors.lastName,
+          errors.message,
+          register,
+        ]
+      )}
+      <div className="flex justify-between items-center">
+        <Checkbox
+          isSelected={isAccetpedRules}
+          onValueChange={(newValue) => setIsAcceptedRules(newValue)}
+          classNames={{
+            label: ["text-sm text-gray-60"],
+          }}
+        >
+          I agree with Terms of Use and Privacy Policy
+        </Checkbox>
+        <Button
+          className="text-white bg-red-45 dark:bg-red-45/80"
+          type="submit"
+          isDisabled={!isAccetpedRules}
+        >
+          Send Message
+        </Button>
+      </div>
+    </form>
+  );
 }
