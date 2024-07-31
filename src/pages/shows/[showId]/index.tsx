5e19b9fdf4dc025ac63f4656a404797a276b59f8
mohammadNosratii <mohammadnosratii2002@gmail.com>
2024-07-31 23:36:08 +0330

    fix vercel errors

--- src/pages/shows/[showId]/index.tsx
+++ src/pages/shows/[showId]/index.tsx
@@ -8,33 +8,33 @@ import MoviesCastsSlider from "../../../components/Modules/Movie&ShowPageInfo/Mo
 import MovieComment from "../../../components/Modules/Movie&ShowPageInfo/MovieComment";
 
 export default function ShowDetailsPage() {
-    const params = useParams({ from: "/shows/$showId" });
+  const params = useParams({ from: "/_main/shows/$showId" });
 
-    const getShowData = allShowsInfo().find(
-        (show) => show.id === Number(params.showId)
-    );
+  const getShowData = allShowsInfo().find(
+    (show) => show.id === Number(params.showId)
+  );
 
-    // TODO should design if not found the movie or show
-    if (!getShowData) {
-        return <div>Show not found</div>;
-    }
-    return (
-        <div>
-            <MovieBanner movieData={getShowData} />
-            <div className="container flex flex-col lg:flex-row items-start gap-5 dark:text-white my-20">
-                <div className="space-y-5 w-full lg:w-[63%]">
-                    <MovieDescription movieData={getShowData} />
-                    <div className="lg:hidden">
-                        <MovieDetail movieData={getShowData} />
-                    </div>
-                    <DownloadLink movieData={getShowData} />
-                    <MoviesCastsSlider movieData={getShowData} />
-                    <MovieComment movieData={getShowData} />
-                </div>
-                <div className="hidden lg:block">
-                    <MovieDetail movieData={getShowData} />
-                </div>
-            </div>
+  // TODO should design if not found the movie or show
+  if (!getShowData) {
+    return <div>Show not found</div>;
+  }
+  return (
+    <div>
+      <MovieBanner movieData={getShowData} />
+      <div className="container flex flex-col lg:flex-row items-start gap-5 dark:text-white my-20">
+        <div className="space-y-5 w-full lg:w-[63%]">
+          <MovieDescription movieData={getShowData} />
+          <div className="lg:hidden">
+            <MovieDetail movieData={getShowData} />
+          </div>
+          <DownloadLink movieData={getShowData} />
+          <MoviesCastsSlider movieData={getShowData} />
+          <MovieComment movieData={getShowData} />
         </div>
-    )
+        <div className="hidden lg:block">
+          <MovieDetail movieData={getShowData} />
+        </div>
+      </div>
+    </div>
+  );
 }
