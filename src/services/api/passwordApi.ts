bb3feeb944061ca7b1590d808edeace3efdd4e3f
Aryan kholqi <aryankholqi@gmail.com>
2024-08-04 20:15:06 +0330

    implement change password api

--- src/services/api/passwordApi.ts
+++ src/services/api/passwordApi.ts
@@ -5,5 +5,5 @@ const baseUrl = import.meta.env.VITE_BASE_DJANGO_API;
 const baseEndPoint = "account/password/";
 
 export const changePasswordApi = (payload: changePasswordProps) => {
-  return httpService.post(baseUrl + baseEndPoint + "change", payload);
+  return httpService.post(baseUrl + baseEndPoint + "change/", payload);
 };
